33bed98be2b9148d210ea930c192116573daea19
ccrismota <ccrismota@gmail.com>
2021-12-03 18:29:35 -0300

    interacted part of BD queries

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,7 +1,8 @@
+from sqlite3.dbapi2 import Cursor
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 from flask_jwt_extended import jwt_required
-import sqlite3 as slq
+import sqlite3 as sql
 
 def normalize_path_params(cidade = None, 
                           estrelas_min = 0, 
@@ -42,10 +43,39 @@ path_params.add_argument('offset',type=float)
 
 class Hoteis(Resource):
     def get(self):
+        connection = sql.connect('banco.db')
+        cursor = connection.cursor()
 
         dados = path_params.parse_args()
         dados_validos = {chave: dados[chave] for chave in dados if dados[chave] is not None}
-        return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]} #SELECT * FROM hoteis
+        paramentros = normalize_path_params(**dados_validos)
+
+        if not paramentros.get('cidade'):
+            consulta = "SELECT * FROM hoteis \
+            WHERE (estrelas >= ? and estrelas <= ?) \
+            and (diaria >= ? and diaria <= ?) \
+            LIMIT ? OFFSET ?"
+            tupla = tuple([paramentros[chave] for chave in paramentros])
+            resultado = cursor.execute(consulta, tupla)
+        else:
+            consulta = "SELECT * FROM hoteis \
+            WHERE (estrelas >= ? and estrelas <= ?) \
+            and (diaria >= ? and diaria <= ?) \
+            and cidade = ? LIMIT ? OFFSET ?"
+            tupla = tuple([paramentros[chave] for chave in paramentros])
+            resultado = cursor.execute(consulta, tupla)
+
+        hoteis = []
+        for linha in resultado:
+            hoteis.append({
+                'hotel_id': linha[0],
+                'nome': linha[1],
+                'estrelas': linha[2],
+                'diaria': linha[3],
+                'cidade': linha[4]
+            })
+
+        return {'hoteis': hoteis} #{'hoteis': [hotel.json() for hotel in HotelModel.query.all()]} #SELECT * FROM hoteis
 
 class Hotel(Resource):
     atributos = reqparse.RequestParser()
